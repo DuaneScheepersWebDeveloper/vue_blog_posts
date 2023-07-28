ac3a57734e4384dada56fc92c7540dabd5faa96f
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-07-28 12:54:00 +0200

    Updated the Vue Files
    Added routers

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,59 @@
 <template>
-    <h1>This is our root component</h1>
+    <div id="wrapper">
+        <!-- header -->
+        <header class="header">
+            <div class="header-text">
+                <h1>Duane Scheepers</h1>
+                <h4>Home of verified news...</h4>
+            </div>
+            <div class="overlay"></div>
+        </header>
+
+        <!-- sidebar -->
+        <div class="sidebar">
+            <span class="closeButton">&times;</span>
+            <p class="brand-title"><a href="">Duane Scheepers</a></p>
+
+            <div class="side-links">
+                <ul>
+                    <li><a class="active" href="index.html">Home</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+                <div>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                </div>
+
+                <small>&copy; 2023 Duane Scheepers Blog</small>
+            </footer>
+        </div>
+        <!-- Menu Button -->
+        <div class="menuButton">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+
+        <!-- main -->
+        <main class="container">
+            <!-- render component -->
+        </main>
+
+        <!-- Main footer -->
+        <footer class="main-footer">
+            <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>&copy; 2023 Duane Scheepers Blog</small>
+        </footer>
+    </div>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
+//php artisan serve
 import "./bootstrap";
 import { createApp } from "vue";
+import "../css/app.css";
 import App from "../js/App.vue";
 createApp(App).mount("#app");
